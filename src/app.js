852c2bb9b8157219f64e016abf10195938bfde8e
jantgg <smosh360@hotmail.com>
2022-11-21 18:41:25 +0000

    excuse1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let item1 = who[Math.floor(Math.random() * 4)];
+  let item2 = action[Math.floor(Math.random() * 4)];
+  let item3 = what[Math.floor(Math.random() * 3)];
+  let item4 = when[Math.floor(Math.random() * 5)];
+  let excuse = item1 + " " + item2 + " " + item3 + " " + item4 + ".";
+
+  document.querySelector("#greeting").innerHTML = excuse;
+  console.log("Hello World");
 };
